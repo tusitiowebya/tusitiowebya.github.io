67137b868851403a769a273f66d0a6586e90ccc1
JotaVPS <joacolomas2@gmail.com>
2026-04-17 15:10:38 -0300

    turbos

---
+++ public_html/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<link rel="stylesheet" type="text/css" href="css/ferozo.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+</head>
+<body>
+
+	<div class="menu-sup">
+		<div class="width_wrap">
+			<a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/ferozo'; ?>" target="_blank"><div class="logo sprite"></div></a>
+	    </div>
+	</div>
+
+	<header>
+		<div class="width_wrap">
+			<div class="bloque-texto">
+				<h2>¡Bienvenido!</h2>
+				<h1>Su cuenta de hosting ha sido creada</h1>
+			</div>
+
+			<div class="img">
+				<img src="img/servidor.gif">			
+			</div>
+	    </div>
+	</header>
+
+	<section class="bloques">
+		<div class="width_wrap">
+
+			<div class="item">
+				<div class="sprite auto"></div>
+				<p>Esta página es colocada automáticamente para indicar que la cuenta de hosting ha sido creada con éxito.</p>
+			</div>
+
+			<div class="item">
+				<div class="sprite ftp"></div>
+				<p>Podrá modificar este contenido alojando los archivos de su sitio en la carpeta public_html/ de su cuenta.</p>
+			</div>
+
+			<div class="item">
+				<div class="sprite panel"></div>
+				<p>Para acceder a su panel de control haga <a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/ferozo'; ?>" target="_blank">click aquí</a> y utilice los datos de acceso que recibió de su proveedor.</p>
+			</div>
+
+		</div>
+	</section>
+
+	<footer>
+		<div class="width_wrap">
+			<h3>Administra tu cuenta de hosting directo desde el panel Ferozo:</h3>
+
+			<p>Parkeo de dominios / Sub-Dominios | Administración de Cuentas de E-mail | Información de la cuenta y el servidor | Cuentas de FTP | Cambio de contraseña e idioma | Administración de Bases de Datos | Backups / Redireccionamientos / Restricciones | CGI-Scripts | Extensiones de FrontPage | Automatización de tareas | Manejadores de Indices / Bloqueos de IP's | Estadísticas | Pack Amigo</p>
+
+			<p class="copyright">Ferozo Panel de Control de Hosting - Todos los derechos Reservados</p>
+
+		</div>
+	</footer>
+
+</body>
+</html>
